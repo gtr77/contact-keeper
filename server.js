bdbafb8a422f4e3d6fc75b036250b4d699a0fdde
Yecid Gustavo <yecid.gustavo.x.romero.higuera.-nd@disney.com>
2022-01-31 11:45:34 -0500

    Set Jason Web Token

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+// CONNECT DATABSE
+connectDB();
+
+// INIT MIDDLEWARE
+app.use(express.json({ extended: false }))
 
 app.get("/", (req, res) => {
   res.json({ description: "Welcome GET"})
@@ -12,5 +19,7 @@ app.use('/api/users', require('./routes/users.js'))
 app.use('/api/auth', require('./routes/auth.js'))
 app.use('/api/contacts', require('./routes/contacts.js'))
 
-app.listen(PORT, () => ( console.log(`Server started at Port ${PORT}`)));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started at Port ${PORT}`));
 
