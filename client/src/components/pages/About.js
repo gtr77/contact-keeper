707d827f551d73fe22f06a55fd6c38858474c448
Yecid Gustavo <yecid.gustavo.x.romero.higuera.-nd@disney.com>
2022-04-29 18:25:13 -0500

    Add/Delete

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className=''>
+      <h1>
+        About this App
+      </h1>
+      <p className="my-1">
+        Full Stack React App - Keeping Contacts
+      </p>
+      <p className="bg-dark p">
+        <strong>Version 1.0.0</strong>
+      </p>
+    </div>
+  )
+}
+export default About;
